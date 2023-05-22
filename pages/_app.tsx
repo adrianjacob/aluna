30df7b1131ca75b4a1104664a2e64fe468a1a7f4
ajacob2 <adrianjacob@gmail.com>
2023-05-22 20:33:30 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Session } from "next-auth";
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 interface MyAppProps extends AppProps {
   pageProps: {
@@ -11,6 +12,10 @@ interface MyAppProps extends AppProps {
 const App = ({ Component, pageProps }: MyAppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>UKDoors.net</title>
+        {/* <meta name="theme-color" content="#f0f0f0" /> */}
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
